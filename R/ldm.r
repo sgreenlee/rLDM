1bb9b9cbb2fa0096e7330240b504566c0fbf9f70
Sam Greenlee <sam.a.greenlee@gmail.com>
2014-10-27 19:48:55 -0400

    added scaling to ldm.default and predict.ldm

--- R/ldm.r
+++ R/ldm.r
@@ -2,7 +2,7 @@
 ldm <- function(x, ...) UseMethod("ldm")
 
 ldm.default <- function(y, X, kernel = 'radial', cost = 10, gamma=1, degree=2, 
-                        a=1, c=1, r=0, lambda_1=1/4, lambda_2=1/4)
+                        a=1, c=1, r=0, lambda_1=1/4, lambda_2=1/4, scale=T)
 {
   kernel_type <- pmatch(kernel, c('linear', 'polynomial', 'radial', 'sigmoid')) - 1
   
@@ -10,7 +10,18 @@ ldm.default <- function(y, X, kernel = 'radial', cost = 10, gamma=1, degree=2,
   if (length(levels(y)) != 2) stop("y must be a two-level factor: multi-class not implemented yet")
   else y <- c(-1,1)[unclass(y)]
   
-  X <- scale(X)
+  if (length(scale)==1) scale <- rep(scale, ncol(X))
+  else if (length(scale) != ncol(X)) {
+    warning("Data not scaled: length of 'scale' argument must be either 1 or the number of columns of X")
+    scale <- FALSE
+  }
+  
+  x.scale <- NULL
+  if (any(scale)) {
+    tmp <- scale(X[,scale])
+    X[,scale] <- tmp
+    x.scale <- attributes(tmp)[c("scaled:center", "scaled:scale")]
+  }
   
   tuning_params <- c(r, a, c, degree, gamma)
   n <- dim(X)[1]
@@ -48,6 +59,8 @@ ldm.default <- function(y, X, kernel = 'radial', cost = 10, gamma=1, degree=2,
   ret$gamma <- gamma
   ret$degree <- degree
   ret$model.matrix <- X
+  ret$scaled <- scale
+  ret$x.scale <- x.scale
   class(ret) <- 'ldm'
   return(ret)
 }
--- R/predict.R
+++ R/predict.R
@@ -1,6 +1,8 @@
-predict.ldm <- function(object, newdata, type=c("response", "decision.values")) {
+predict.ldm <- function(object, newdata, type="response") {
   
   if(missing(newdata)) return(object$fitted)
+  
+  type <- c("response", "decision.values")[pmatch(type, c("response", "decision.values"))]
   
   ### ONLY WORKS FOR RBF KERNEL A.T.M. ###
   
@@ -10,14 +12,18 @@ predict.ldm <- function(object, newdata, type=c("response", "decision.values")) 
   
   }
   
-  ####                ####
-  #       SCALE DATA     #
-  ####                ####
-  
   alpha <- object$alpha
   mod.matrix <- object$model.matrix
   d <- c(dim(newdata)[1], dim(mod.matrix))
   gamma <- object$gamma
+  
+  scaled <- object$scaled
+  
+  if (any(scaled)) {
+    x.scale <- object$x.scale
+    xtmp <- scale(newdata[ , scaled], center=x.scale[[1]], scale=x.scale[[2]])
+    newdata[ , scaled] <- xtmp
+  }
   
   pred <- .C("ldmPredict", as.double(alpha), as.double(gamma), 
              as.double(newdata), as.integer(d), as.double(mod.matrix), 
